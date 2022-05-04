37fd7692040ae667f7e3002348e5f83be1c5a7df
Philipp Ganster <85439878+pgphi@users.noreply.github.com>
2022-05-04 14:56:46 +0200

    Version 1.0

---
+++ main.py
@@ -0,0 +1,4 @@
+from transcription import transcribe
+
+if __name__ == '__main__':
+    transcribe()
---
+++ transcription.py
@@ -0,0 +1,80 @@
+import os
+import requests
+
+def transcribe ():
+
+    while True:
+
+        try:
+            # 1 Get Audiofile
+            print("type in the filename of the file in the directory 'Audios' of this i.e. 'Audio.m4a': ")
+            name = input()
+            filename = os.getcwd() + "/Audios/" + name
+
+            print("The Audiofile " + name + " is processed ... ")
+
+
+            # 2 Upload Audiofile to AssemblyAI Server
+            def read_file(filename, chunk_size=5242880):
+                with open(filename, "rb") as _file:
+                    while True:
+                        data = _file.read(chunk_size)
+                        if not data:
+                            break
+                        yield data
+
+
+            api_key = "ef8e04f3a41f4af28ac8b884537b1a83"
+
+            headers = {"authorization": api_key}
+            response = requests.post("https://api.assemblyai.com/v2/upload",
+                                     headers=headers,
+                                     data=read_file(filename))
+
+            audio_url = response.json()["upload_url"]
+            # print(audio_url)
+
+            # 3 Transcribe the Audiofile
+            endpoint = "https://api.assemblyai.com/v2/transcript"
+
+            print("Type in your language code further see https://docs.assemblyai.com/#supported-languages: ")
+            lang_code = input()
+            print(type(lang_code))
+
+            json = {
+                "audio_url": audio_url,
+                "language_code": lang_code
+            }
+
+            headers = {
+                "authorization": api_key,
+                "content-type": "application/json"
+            }
+
+            transcript_input_response = requests.post(endpoint, json=json, headers=headers)
+            # print(transcript_input_response.json())
+
+            # 4 Retrieve Transcription Result
+            transcript_id = transcript_input_response.json()["id"]
+            # print(transcript_id)
+
+            endpoint = f"https://api.assemblyai.com/v2/transcript/{transcript_id}"
+
+            headers = {
+                "authorization": api_key,
+            }
+
+            transcript_output_response = requests.get(endpoint, headers=headers)
+            print("Process finished. Here is the Transcription of " + name + ":")
+            print(transcript_output_response.json()["text"])
+            print("Do you want to continue? Type 'True' for Yes and type 'False' for No.")
+            run = input()
+
+            if run == "True":
+                continue
+
+            else:
+                print("Program finished - until next time :)!.")
+                break
+        except:
+            print("Ups, something went wrong. Start the program again and exactly follow the instructions :).")
